cd81749d8c731f992b9554ed77502b6463d21c09
ManuhzinhaCor13 <manuellacorinaraujo@gmail.com>
2024-11-21 12:12:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,44 @@
+criaCartao(
+    'Programação da Manuh',
+    'Qual a comida favorita da Manuh?',
+    'Escondidinho de carne seca com mandioca'
+)
+
+criaCartao(
+    'Programação da Manuh',
+    'Qual o briquendo favorito da Manuh no Hopi Hari ?',
+    'Katapul'
+)
+
+criaCartao(
+    'Programação da Manuh',
+    'O que a Manuh quer cursar?',
+    'Manuh quer cursar Teatro'
+)
+
+criaCartao(
+    'Programação da Manuh',
+    'Qual a cor dos olhos da Manuh?',
+    'Alguns discordam mas os olhos da Manuh säo Verdes'
+)
+
+criaCartao(
+    'Programação da Manuh',
+    'Qual o filme favorito da Manuh?',
+    'Ela é indecisa até nisso! Os filmes que ela considera os melhores säo: Como eu era antes de vocë, A Trilogia de HSM e A saga de jogos vorazes'
+)
+
+criaCartao(
+    'Programação da Manuh',
+    'Qual o tipo de unha favorito da Manuh?',
+    'Amendoadas'
+)
+
+criaCartao(
+    'Programação da Manuh',
+    'Qual artista favorito da Manuh?',
+    'Taylor Swift'
+)
+
+
+
